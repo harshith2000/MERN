370c680e0f7f85756c3bb2af45718433c8598b0e
saiharshithkaruneegarramesh <sai.harshith108@gmail.com>
2023-07-09 16:41:04 -0500

    Connected Blog site to DB

--- Blog site/ejs-challenge/app.js
+++ Blog site/ejs-challenge/app.js
@@ -3,13 +3,12 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const mongoose = require("mongoose");
 const _ = require("lodash");
 
 let posts = [];
 
 const homeStartingContent = "Welcome to my blog!";
-const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
-const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
 const app = express();
 
@@ -18,42 +17,60 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
+mongoose.connect("mongodb://localhost:27017/blogDB", {useNewUrlParser: true});
 
-app.get("/", function(req, res){
-  res.render("home", {homeContent: homeStartingContent, posts: posts});
-});
+const blogSchema = {
+  title: String,
+  post: String
+};
 
-app.get("/about", function(req, res){
-  res.render("about", {aboutContent: aboutContent});
-});
+const Blog = mongoose.model("Blog", blogSchema);
 
-app.get("/contact", function(req, res){
-  res.render("contact", {contactContent: contactContent});
+
+app.get("/", async function(req, res){
+  await Blog.find().then(function(blogs){
+    res.render("home", {homeContent: homeStartingContent, posts: blogs});
+  });
 });
 
 app.get("/compose", function(req, res){
   res.render("compose");
 });
 
-app.get("/posts/:post", function(req, res){
-  let paramstitle = _.lowerCase(req.params.post);
-  posts.forEach(function(post){
-    let title = _.lowerCase(post.title);
-    if(title === paramstitle){
-      res.render("post", {title: post.title, post: post.post});
-    };
-  });
+app.get("/posts", function(req, res){
   res.redirect("/");
+});
+
+app.get("/posts/:post", async function(req, res){
+  let paramstitle = _.lowerCase(req.params.post);
+  let flag = false;
+  await Blog.find().then(function(blogs){
+    blogs.forEach(function(blog){
+      let dbBlogtitle = _.lowerCase(blog.title);
+      if(dbBlogtitle == paramstitle) {
+        flag = true;
+        res.render("post", {title: blog.title, post: blog.post});
+      };
+    });
+  })
+  if(!flag) {
+    res.send(`404! No ${req.params.post} blog found :(`);
+  }
 });
 
 
 
 app.post("/", function(req, res){
-  let post = {
-    title: req.body.title,
-    post: req.body.postContent
-  };
-  posts.push(post);
+  let title = req.body.title;
+  let post = req.body.postContent;
+  if(post.title === "" || post.post === "") {
+    res.redirect("/compose");
+  } 
+  const blog = new Blog({
+    title: title,
+    post: post
+  });
+  blog.save();
   res.redirect("/");
 });
 
